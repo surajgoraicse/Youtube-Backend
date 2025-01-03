7e82ed1b1731e423405c3106aa7c0712ef2c19a8
surajgoraicse <surajgoraicse@gmail.com>
2025-01-03 20:56:11 +0530

    playlist schema

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -2,7 +2,22 @@ import mongoose, { Schema } from "mongoose"
 
 const playlistSchema = new Schema(
     {
-        
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        videos: [{
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }],
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref : "User"
+        }
 
     }
     , {
