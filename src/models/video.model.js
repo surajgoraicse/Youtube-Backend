3e3a77c3e307945fd97bdd512da05ccba79fed90
surajgoraicse <surajgoraicse@gmail.com>
2025-01-03 20:59:02 +0530

    models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,4 @@
 import mongoose, { Schema, model } from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
     videoFile: {
@@ -28,7 +27,7 @@ const videoSchema = new Schema({
     },
     views: {
         type: Number,
-        required: true
+        default: 0
     },
     isPublished: {
         type: Boolean,
